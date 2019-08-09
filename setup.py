f9bc25a728435dbcd4b5c12631b2eacb6ea2130e
Lefteris <el.avramidis@gmail.com>
2019-08-09 21:16:18 +0100

    Added setup file

---
+++ setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+
+setup(name='fastseriation',
+      version='1.0.0',
+      description='Python package that can be used to seriate matrices. ',
+      packages=['fastseriation'],
+      license='MIT'
+      )
