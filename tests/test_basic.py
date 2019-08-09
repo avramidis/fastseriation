c3e17f0e0387ff7016fb930040482ec66fd782ea
Lefteris <el.avramidis@gmail.com>
2019-08-09 21:16:05 +0100

    Corrections to the integration test

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -1,30 +1,36 @@
 import unittest
 import sys
+import numpy
 
 sys.path.append('../fastseriation')
-import fastseriation.covert_raw_to_rank_matrix
-import fastseriation.get_similarity_matrix
 import fastseriation.seriate
-import numpy
 
 
 class TestFastSeriationMethods(unittest.TestCase):
 
     def test_fastseriation_1(self):
 
-        scores_original = numpy.zeros((10000, 10))
-        for i in range(100):
-            for j in range(10):
-                scores_original[i, j] = abs(i - j)
+        nrows = 10
+        ncols = 2
 
-        per = numpy.random.permutation(10000)
-        scores = scores_original[per, :]
+        scores = numpy.zeros((nrows, ncols))
+        for i in range(nrows):
+            for j in range(ncols):
+                scores[i, j] = abs(i - j)
 
-        R = fastseriation.covert_raw_to_rank_matrix.convert_raw_to_rank_matrix(scores)
-        S = fastseriation.get_similarity_matrix.get_similarity_matrix(R)
-        scores_seriated = fastseriation.seriate.seriate(S)
+        per = numpy.random.permutation(nrows)
+        scores_pertutated = scores[per, :]
+        seriated_indexes = fastseriation.seriate.seriate(scores_pertutated)
 
-        self.assertEqual(1, 1)
+        result = False
+
+        if numpy.array_equiv(scores, scores_pertutated[seriated_indexes, :]):
+            result = True
+
+        if numpy.array_equiv(scores, numpy.flipud(scores_pertutated[seriated_indexes, :])):
+            result = True
+
+        self.assertTrue(result)
 
 
 if __name__ == '__main__':
