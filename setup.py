ccb747b1388b1171648610c68a4456972ebf06d5
Lefteris <el.avramidis@gmail.com>
2019-08-10 08:20:25 +0100

    Updated misc files

--- setup.py
+++ setup.py
@@ -2,7 +2,9 @@ from distutils.core import setup
 
 setup(name='fastseriation',
       version='1.0.0',
-      description='Python package that can be used to seriate matrices. ',
+      description='Python package that can be used to seriate matrices.',
+      author='Eleftherios Avramidis',
+      author_email='el.avramidis@gmail.com',
       packages=['fastseriation'],
       license='MIT'
       )
